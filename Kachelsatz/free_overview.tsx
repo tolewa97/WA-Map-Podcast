f998978b460a7828ef57e1fd8a81515bcd3e9f14
tolewa <tolewa97@gmail.com>
2023-07-04 19:48:26 +0200

    Fixed Bug
    bug, where file for upper area are not loading, fixed

---
+++ Kachelsatz/free_overview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="free_overview" tilewidth="32" tileheight="32" tilecount="3248" columns="58">
+ <image source="../tilesets/free_overview.png" width="1886" height="1792"/>
+</tileset>
