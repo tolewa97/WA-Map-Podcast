b34ce002694bfdd930c90aa0dbec895cd5c0c5f9
tolewa <tolewa97@gmail.com>
2023-07-04 18:24:11 +0200

    Changed upper Floor
    visual changes done, functions are missing at the moment

---
+++ tilesStuff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tilesStuff" tilewidth="32" tileheight="32" tilecount="592" columns="37">
+ <image source="C:/Users/tolewa/Desktop/tiles_laboratory_LandOfPixels/tiles_laboratory_LandOfPixels/32px/tilesStuff.png" width="1184" height="512"/>
+</tileset>
