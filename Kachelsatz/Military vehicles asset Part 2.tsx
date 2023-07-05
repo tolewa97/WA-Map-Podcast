f2f3cbbffe23965ba981f8f0b8cca8101eedfb3d
tolewa <tolewa97@gmail.com>
2023-07-06 00:44:42 +0200

    Dekoration mostly done
    dekoration lower area nearly done, just some small changes left

---
+++ Kachelsatz/Military vehicles asset Part 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Military vehicles asset Part 2" tilewidth="32" tileheight="32" tilecount="196" columns="14">
+ <image source="../tilesets/Military vehicles asset Part 2.png" width="450" height="450"/>
+</tileset>
---
+++ Kachelsatz/spr_train_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="spr_train_0" tilewidth="32" tileheight="32" spacing="6" tilecount="16" columns="4">
+ <image source="../tilesets/spr_train_0.png" width="150" height="150"/>
+</tileset>
