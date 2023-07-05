bb21afcd21da8f6a2491ae7566ecc6f5dfffa5de
tolewa <tolewa97@gmail.com>
2023-07-06 01:05:27 +0200

    Lower Area Dekoration done
    the dekoration of the lower area is complete now

---
+++ Kachelsatz/spr_classiccar_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="spr_classiccar_0" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="../tilesets/spr_classiccar_0.png" width="200" height="73"/>
+</tileset>
