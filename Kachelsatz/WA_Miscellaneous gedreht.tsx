1e08bfa3c2b6c8d50ce252dca65a41d2b554d35b
tolewa <tolewa97@gmail.com>
2023-07-10 20:39:32 +0200

    Added more dekoration
    added dekoration for new interactiv areas

---
+++ Kachelsatz/WA_Miscellaneous gedreht.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="WA_Miscellaneous gedreht" tilewidth="32" tileheight="32" spacing="7" margin="4" tilecount="72" columns="8">
+ <image source="../tilesets/WA_Miscellaneous gedreht.png" width="320" height="352"/>
+</tileset>
