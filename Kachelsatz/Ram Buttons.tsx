146433a294b9a43908ecaadd781f048ed6327359
tolewa <tolewa97@gmail.com>
2023-07-10 19:41:58 +0200

    QOL changes Upper Area
    added some arrows on the floor for better understanding

---
+++ Kachelsatz/Ram Buttons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Ram Buttons" tilewidth="32" tileheight="32" tilecount="989" columns="43">
+ <image source="../tilesets/Ram Buttons.png" width="1376" height="736"/>
+</tileset>
