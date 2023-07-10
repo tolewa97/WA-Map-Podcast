e78125b922d8b073edc209f4c88bdce2e2395b6c
tolewa <tolewa97@gmail.com>
2023-07-10 20:23:21 +0200

    Added Podcast Areas
    added some podcast areas for more exaple podcasts

---
+++ Kachelsatz/MegaPixelArt32x32pxIcons_SpriteSheet Quer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="MegaPixelArt32x32pxIcons_SpriteSheet Quer" tilewidth="32" tileheight="32" tilecount="504" columns="36">
+ <image source="../tilesets/MegaPixelArt32x32pxIcons_SpriteSheet Quer.png" width="1152" height="448"/>
+</tileset>
